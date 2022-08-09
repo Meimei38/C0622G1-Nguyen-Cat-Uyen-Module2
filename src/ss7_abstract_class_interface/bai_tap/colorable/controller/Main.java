4ec0c1bd7689fb4a9b447f00fac547eafe083b85
Meimei38 <uyennguyencat@gmail.com>
2022-08-09 20:25:20 +0700

    nop bai ngay 9/8

---
+++ src/ss7_abstract_class_interface/bai_tap/colorable/controller/Main.java
@@ -0,0 +1,6 @@
+/*
+package ss7_abstract_class_interface.bai_tap.colorable.controller;
+
+public class Main {
+}
+gvhbjnkm*/
---
+++ src/ss7_abstract_class_interface/bai_tap/colorable/model/Shape.java
@@ -0,0 +1,6 @@
+/*
+package ss7_abstract_class_interface.bai_tap.colorable.model;
+
+public class Shape {
+}
+ghjkml*/

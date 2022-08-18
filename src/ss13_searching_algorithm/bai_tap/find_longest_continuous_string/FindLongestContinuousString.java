b10fc985ee4d85383c71cffb73d77aabf16246b0
Meimei38 <uyennguyencat@gmail.com>
2022-08-18 11:03:59 +0700

    update

--- src/ss13_searching_algorithm/bai_tap/find_longest_continuous_string/FindLongestContinuousString.java
+++ src/ss13_searching_algorithm/bai_tap/find_longest_continuous_string/FindLongestContinuousString.java
@@ -6,24 +6,30 @@ import java.util.Scanner;
 public class FindLongestContinuousString {
     private static Scanner scanner = new Scanner(System.in);
 
+
     public static void main(String[] args) {
-        String inputString = "abcdabhijabdfgk";
+        String inputString = "abcabcdgabxyabcdg";
         LinkedList<Character> maxList = new LinkedList();
         LinkedList<Character> list = new LinkedList();
 
         for (int i = 0; i < inputString.length(); i++) {
-            list.add(inputString.charAt(0));
-            for (int j = i + 1; j < inputString.length(); j++) {
-                if (inputString.charAt(i) > list.getLast()) {
-                    list.add(inputString.charAt(i));
-
-                }
-
+            if (list.size() > 1 && inputString.charAt(i) <= list.getLast() && list.contains(inputString.charAt(i))) {
+                list.clear();
+            }
+            list.add(inputString.charAt(i));
+            if (list.size() > maxList.size()) {
+                maxList.clear();
+                maxList.addAll(list);
             }
 
 
         }
 
+        for (Character ch : maxList) {
+            System.out.print(ch);
+
+        }
+        System.out.println();
 
 
     }

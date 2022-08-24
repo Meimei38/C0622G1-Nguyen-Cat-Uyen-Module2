5dfe9a1a2d62f24ffe8552e3a87184ecd3d39424
Meimei38 <uyennguyencat@gmail.com>
2022-08-24 21:33:33 +0700

    update

--- src/ss17_binary_serialization/bai_tap/copy_binary_file/CopyBinaryFile.java
+++ src/ss17_binary_serialization/bai_tap/copy_binary_file/CopyBinaryFile.java
@@ -37,6 +37,8 @@ public class CopyBinaryFile {
             FileInputStream fileInputStream = new FileInputStream(file);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
             lines = (List<String>) objectInputStream.readObject();
+            fileInputStream.close();
+            objectInputStream.close();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -72,7 +74,8 @@ public class CopyBinaryFile {
         return inputList;
 
     }
-    public static int countByte(String path){
+
+    public static int countByte(String path) {
         int size = 0;
         try {
             FileInputStream fileInputStream = new FileInputStream(path);
@@ -92,14 +95,10 @@ public class CopyBinaryFile {
         String copiedFile = "src\\ss16_io_text_file\\bai_tap\\copy_file_text\\data\\copy_target_file.dat";
 
         List<String> content = readSourceFile(sourceFile); //Đọc file ký tự
-        writeFile(targetFile,content); // Viết vào file nhị phân
+        writeFile(targetFile, content); // Viết vào file nhị phân
         List<String> copiedContent = readBinaryFile(targetFile); // Đọc file nhị phân
-        writeFile(copiedFile,copiedContent); // Copy bản file nhị phân vào 1 file khác
-        System.out.printf("The size of copied file is: %d bytes",countByte(copiedFile));
-
-
-
-
+        writeFile(copiedFile, copiedContent); // Copy bản file nhị phân vào 1 file khác
+        System.out.printf("The size of copied file is: %d bytes", countByte(copiedFile));
 
 
     }
